ecf90b4ebd39990fca173cf7156bebb6eedafa2e
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-05-19 21:39:27 +0100

    no assert

--- build_pages.py
+++ build_pages.py
@@ -13,4 +13,4 @@ os.system(f"cp {this_dir}/static/* {config.output_dir}")
 for file in os.listdir(f"{this_dir}/pages"):
     if file.endswith(".py") and not file.startswith("_"):
         print(f"Running {file}")
-        assert os.system(f"PYTHONPATH=\"$PYTHONPATH:{this_dir}\" python3 pages/{file}")
+        os.system(f"PYTHONPATH=\"$PYTHONPATH:{this_dir}\" python3 pages/{file}")
