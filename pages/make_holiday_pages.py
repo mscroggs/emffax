b3d5367168e218d2680dda0311375e8ed6173502
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-05-21 16:55:41 +0100

    correct colours

--- pages/make_holiday_pages.py
+++ pages/make_holiday_pages.py
@@ -21,7 +21,7 @@ line.start_bg(Color.BLACK)
 p.set_line(3, line)
 
 line = Line()
-line.start_bg(Color.YELLOW)
+line.start_bg(Color.BLUE)
 line.add_block("""
 xxxxxxxxxxxxxxx....x.........x....x........x....x.......................
 xxxxxxxxxxxxxxx..xxx.xxxxxxx.x..xxx.xxxxxx.x..x.x.xxxxxxxxxxxxxxxxxxxxxx
@@ -31,7 +31,7 @@ line.start_bg(Color.BLACK)
 p.set_line(4, line)
 
 line = Line()
-line.start_bg(Color.YELLOW)
+line.start_bg(Color.BLUE)
 line.add_block("""
 xxxxxxxxxxxxxxx..xxx.x.....x.x..xxx.x..xxx.x..x.x.x..x..xxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxx....x.x.x.x.x.x..xxx.x...xx.x..x.x.xx...xxxxxxxxxxxxxxxxx
@@ -41,7 +41,7 @@ line.start_bg(Color.BLACK)
 p.set_line(5, line)
 
 line = Line()
-line.start_bg(Color.YELLOW)
+line.start_bg(Color.BLUE)
 line.add_block("""
 .....................x.xxx.x........x..xxx........x..x..xxxxxxxxxxxxxxxx
 ...................xxxxxxxxx......xxxxxxxx......xxxxxxxxxxxxxxxxxxxxxxxx
