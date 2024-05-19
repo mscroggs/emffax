6aea564986cd76985ba3cdaec8249efacd80d7ad
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-05-19 14:11:25 +0100

    _

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ teletext_button = "TELETEXT_BUTTON"
 open_ldbws_key = None
 
 try:
-    from local_config import *  # noqa: E402, F403, F401
+    from localconfig import *  # noqa: E402, F403, F401
 except ModuleNotFoundError:
     pass
